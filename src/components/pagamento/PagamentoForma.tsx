1f297d7f5b80d833df7ad2bc3e99a59c118dff71
samuel muzy <samuelmuzyfernandes4@gmail.com>
2025-05-15 00:32:42 -0300

    Mudança pagamento

--- src/components/pagamento/PagamentoForma.tsx
+++ src/components/pagamento/PagamentoForma.tsx
@@ -1,17 +1,41 @@
-export const PagamentoForma = () =>{
-
+export const PagamentoForma = () => {
     const URLAPI = import.meta.env.VITE_URLAPI;
-
-    
+  
     const handlePagar = async () => {
-        const res = await fetch(`${URLAPI}/pagamento/criar-preferencia`, { method: 'POST' });
-        const data = await res.json();
-        window.location.href = data.init_point; // Redireciona para o Checkout Pro
-      };
-      
-    return(
-        <div>
-            <button onClick={handlePagar}>Pagar</button>
-        </div>
-    )
-}
+      const res = await fetch(`${URLAPI}/pagamento/criar-preferencia`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: 'Camiseta Preta',
+          quantity: 1,
+          unit_price: 100,
+          payer: {
+            name: 'João da Silva',
+            email: 'joaodasilva-teste@email.com', // TEM QUE ser diferente do vendedor!
+            identification: {
+              type: 'CPF',
+              number: '12345678909',
+            },
+          },
+        }),
+      });
+  
+      const data = await res.json();
+  
+      if (data?.init_point) {
+        window.location.href = data.init_point;
+      } else {
+        alert('Erro ao gerar link de pagamento!');
+        console.error('Erro:', data);
+      }
+    };
+  
+    return (
+      <div>
+        <button onClick={handlePagar}>Pagar</button>
+      </div>
+    );
+  };
+  
